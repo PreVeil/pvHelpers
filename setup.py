68a9060378b42c4ab81709b01209c986a2b7bfd1
burrows <burrows@preveil.com>
2017-01-18 16:56:43 -0500

    GetUserDBSessionAsPreVeil, GetMailDBSessionAsPreVeil
    Bump version to 1.17

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 setup(name='pvHelpers',
-      version='1.16',
+      version='1.17',
       packages=['pvHelpers'],
       install_requires=[
         "PyYAML==3.11",
         "requests==2.7.0",
         "simplejson==3.8.2",
+        "SQLAlchemy==1.1.6.dev0",
       ],
 )
