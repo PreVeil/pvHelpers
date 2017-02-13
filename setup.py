49bc77b23b75e193d74fb77d7d25c89f9d6abd83
burrows <burrows@preveil.com>
2017-02-13 17:10:16 -0500

    Update SQLAlchemy verison
    Bump to 1.19

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 setup(name='pvHelpers',
-      version='1.18',
+      version='1.19',
       packages=['pvHelpers'],
       install_requires=[
         "PyYAML==3.11",
         "requests==2.7.0",
         "simplejson==3.8.2",
-        "SQLAlchemy==1.1.5",
+        "SQLAlchemy==1.1.5.dev0",
       ],
 )
