a821126fb593c017c6b9f501bf9c8d9951ee9084
Hangkun Ung <HangkuN@Hangkuns-MacBook-Pro.local>
2017-06-22 11:27:57 -0400

    mailbox alias in one place

--- pvHelpers/email_util.py
+++ pvHelpers/email_util.py
@@ -671,7 +671,7 @@ class Email(object):
         o["snippet"] = self.snippet
         o["uid"] = self.uid
         o["thread_id"] = self.thread_id
-        o["mailbox_name"] = MAILBOX_ALIAS[self.mailbox_name]
+        o["mailbox_name"] = setMailboxAlias(self.mailbox_name)
         o["mailbox_id"] = self.mailbox_server_id
         o["flags"] = self.flags
         o["message_id"] = self.message_id
@@ -945,3 +945,11 @@ def setMIMEBcc(message, bccs):
         message.headers["Bcc"] = u"{}".format(", ".join([u"{} <{}>".format(bcc["display_name"], bcc["user_id"]) for bcc in bccs]))
 
     return True, mime.from_string(message.to_string())
+
+
+###FIXME: this works when we only have aliases for the 4 default maiboxes,
+# and use the real "DB" mailbox_name for a custom mailbox
+def setMailboxAlias(mailbox_name):
+    if (mailbox_name in MAILBOX_ALIAS):
+        return MAILBOX_ALIAS[mailbox_name]
+    return mailbox_name
