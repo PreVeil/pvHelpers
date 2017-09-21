6226bab45c9802c517cff10781cb1b2d5f67a4d8
Saman Tehrani <samanrtehrani@gmail.com>
2017-09-21 12:17:59 -0400

    bumping v

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name="pvHelpers",
-      version="1.35",
+      version="1.36",
       packages=["pvHelpers", "pvHelpers.email"],
       install_requires=[
         "PyYAML==3.11",
