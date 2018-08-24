7b12b07a5990b45d95dc83106f0e668a9bb1fe5e
hu13 <hangkun@preveil.com>
2018-08-24 17:28:10 -0400

    flanker update to fix quopri

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name="pvHelpers",
-      version="2.90",
+      version="2.95",
       packages=["pvHelpers", "pvHelpers.email", "pvHelpers.crypto", "pvHelpers.crypto.asymm_key",
                 "pvHelpers.crypto.box", "pvHelpers.crypto.user_key", "pvHelpers.crypto.sign_key",
                 "pvHelpers.crypto.symm_key", "pvHelpers.protos"],
@@ -11,7 +11,7 @@ setup(name="pvHelpers",
         "SQLAlchemy==1.1.5.dev0",
         "Werkzeug==0.11.4",
         "libnacl==1.4.5+preveil1",
-        "flanker==0.4.40",
+        "flanker==0.4.40+preveil1.10",
         "pysqlite==2.8.3.1",
         "semver==2.7.7",
         "protobuf==3.5.2.post1"
