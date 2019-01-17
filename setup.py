58982cc07af2bf68cfa8bf09e491bffa18ebec79
hu13 <hangkun@preveil.com>
2019-01-17 12:54:13 -0500

    Test add loguru

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name="pvHelpers",
-      version="4.05",
+      version="4.10",
       packages=["pvHelpers", "pvHelpers.email", "pvHelpers.crypto", "pvHelpers.crypto.asymm_key",
                 "pvHelpers.crypto.box", "pvHelpers.crypto.user_key", "pvHelpers.crypto.sign_key",
                 "pvHelpers.crypto.symm_key", "pvHelpers.protos"],
