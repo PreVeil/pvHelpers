a3175242400d53631cbe8a2229c1276856d676a0
Hangkun Ung <hangkun.ung@gmail.com>
2019-04-08 17:11:57 -0400

    pv4.70: Add (filename #line) to log and only keep 20 log files (#54)
    * filename linenumber to log and only keep 20 logs file
    
    * Clean up frame explicitly
    
    * Tests
    
    * bb

---
+++ tests/g_log_test.py
@@ -0,0 +1,56 @@
+import sys
+import cStringIO
+from pvHelpers import g_log
+
+# capture stdout of g_log
+stdout_ = sys.stdout
+stream = cStringIO.StringIO()
+sys.stdout = stream
+g_log.exception("debug_msg")
+sys.stdout = stdout_  # restore the previous stdout.
+msg = stream.getvalue()
+
+# remove the time stamp since we can't capture that
+assert " ".join(
+    msg.split(" ")[2:]) == "EXCEPTION: (g_log_test.py 9: <module>) debug_msg\n"
+
+
+def test_g_log_caller_info():
+    # capture stdout of g_log
+    stdout_ = sys.stdout
+    stream = cStringIO.StringIO()
+    sys.stdout = stream
+    g_log.debug("debug_msg")
+    sys.stdout = stdout_  # restore the previous stdout.
+    msg = stream.getvalue()
+
+    # remove the time stamp since we can't capture that
+    assert " ".join(
+        msg.split(" ")[2:]
+    ) == "DEBUG: (g_log_test.py 23: test_g_log_caller_info) debug_msg\n"
+
+    def caller():
+        stdout_ = sys.stdout
+        stream = cStringIO.StringIO()
+        sys.stdout = stream
+        g_log.info("debug_msg")
+        sys.stdout = stdout_  # restore the previous stdout.
+        return stream.getvalue()
+
+    assert " ".join(caller().split(" ")
+                    [2:]) == "INFO: (g_log_test.py 36: caller) debug_msg\n"
+
+    def caller2():
+        def inner_func():
+            stdout_ = sys.stdout
+            stream = cStringIO.StringIO()
+            sys.stdout = stream
+
+            g_log.warn("debug_msg")
+            sys.stdout = stdout_  # restore the previous stdout.
+            return stream.getvalue()
+
+        return inner_func()
+
+    assert " ".join(caller2().split(" ")
+                    [2:]) == "WARN: (g_log_test.py 49: inner_func) debug_msg\n"
