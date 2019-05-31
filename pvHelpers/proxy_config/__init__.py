4e090fe6e50b6f801309762dbee304a282980b4c
Hangkun Ung <hangkun@preveil.com>
2019-05-31 18:29:34 -0400

    GG

--- pvHelpers/proxy_config/__init__.py
+++ pvHelpers/proxy_config/__init__.py
@@ -2,9 +2,3 @@ from .proxy_config_util import ProxyConfig, parse_os_proxy_config, process_os_pr
 from .constants import *
 from .proxy_config_item import *
 from .pac_parser import Pac, download_url
-
-# this ensures that we load operating system's
-# cert bundles along with those of certifi
-from certifi_win32.wincerts import generate_pem
-import certifi  # Ensure patch is in place
-generate_pem()
