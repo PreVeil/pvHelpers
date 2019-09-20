16c39815cb7e9f745c739ad691b1b84e3de11190
Saman Tehrani <saman@preveil.com>
2019-09-20 09:41:53 -0400

    bump version

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ if sys.platform == "win32":
 
 setup(
     name="pvHelpers",
-    version="5.0.8",
-    package_dir={'': 'src'},
-    packages=find_packages(where='src', exclude=('tests')),
+    version="6.0.1",
+    package_dir={"": "src"},
+    packages=find_packages(where="src", exclude=("tests")),
     install_requires=install_requires,
 )
